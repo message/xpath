e32011f5ab50803d74f0fad39d3bcf99697c079a
Dmitry Polovka <messaged@messaged.lv>
2014-11-13 01:35:53 +0200

    Initial commit

---
+++ xpath.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : xpath.cpp
+// Author      : Dmitry Polovka
+// Version     :
+// Copyright   : 
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
